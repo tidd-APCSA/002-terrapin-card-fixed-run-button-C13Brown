3dabb86aae2dec2048fa8610f1f9a1e7ff5c71c3
Connor Brown <636765@aacps.org>
2020-11-13 14:35:16 +0000

    nothing

--- src/main/java/CardRunner.java
+++ src/main/java/CardRunner.java
@@ -1,7 +1,19 @@
 public class CardRunner {
-    public static void main(String args[]){
-        // ☕
-        System.out.println("hey");
-
-    }
+public static void main(String args[]){
+TerrapinCard billy = new TerrapinCard (20);
+TerrapinCard brian = new TerrapinCard (30);
+billy.payGourmet();
+brian.payEconomical();
+System.out.println("Billy: " + billy);
+System.out.println("Brian: " + brian);
+billy.loadMoney(20);
+brian.payGourmet();
+System.out.println("Billy: " + billy);
+System.out.println("Brian: " + brian);
+billy.payEconomical();
+billy.payEconomical();
+brian.loadMoney(50);
+System.out.println("Billy: " + billy);
+System.out.println("Brian: " + brian);
+}
 }
--- src/main/java/TerrapinCard.java
+++ src/main/java/TerrapinCard.java
@@ -1,4 +1,39 @@
-public class TerrapinCard {
+public class TerrapinCard{
+  private double balance;
 
+  public TerrapinCard(double theBalance){
+    balance = theBalance;
+  }
+
+public String toString(){
+  return "The card has " + balance + " dollars";
+}
+
+public void payEconomical() {
+  if(balance < 2.5){
+    System.out.println("Insufficient Funds");
+  }
+  else{
+    balance = balance - (double) 2.5;
+  }
+}
+
+public void payGourmet() {
+if(balance < 4){
+    System.out.println("Insufficient Funds");
+  }
+  else{
+    balance-=4;
+  }
+}
+
+public void loadMoney(double amount) {
+  if(amount > 0){
+balance += amount;
+  }
+if(balance > 150){
+  balance = 150;
+}
+}
 
 }
